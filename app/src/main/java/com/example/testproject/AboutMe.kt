45c6059960524b090a4c940746b5093bbe7b51a3
Kelden Mayer <33588903+Mayerkel@users.noreply.github.com>
2019-05-14 23:03:43 -0500

    Added a new Activity and button activity

---
+++ app/src/main/java/com/example/testproject/AboutMe.kt
@@ -0,0 +1,12 @@
+package com.example.testproject
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class AboutMe : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_me)
+    }
+}
